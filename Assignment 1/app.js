2a8d504f8f36ae3505ed446c0a161b4ed695b39f
Junayad Bin Forhad <jbin-forhad@myseneca.ca>
2023-02-15 16:00:48 -0500

    Final

--- Assignment 1/app.js
+++ Assignment 1/app.js
@@ -1,62 +1,68 @@
+
+//// Array of car objects with details such as car type, license plate, availability, and daily rate
 const cars = [
   { type: "SUV", licensePlate: "ABC 124", available: false, dailyRate: 15 },
   { type: "SEDAN", licensePlate: "BXL 009", available: true, dailyRate: 10 },
   { type: "SUV", licensePlate: "KML 155", available: true, dailyRate: 15 },
-];
+  ];
 
-function createReservation(carType, days, seatNeeded) {
-  const car = cars.find((car) => car.type === carType && car.available);
+//// Function to create a car reservation
+const createReservation = (carType, days, seatNeeded) => 
+{
+  const car = cars.find((car) => car.type.toUpperCase() === carType.toUpperCase() && car.available);
   if (!car) {
     console.log(`Sorry, no ${carType} available.`);
     return;
   }
 
-  const rentalFee = days * car.dailyRate;
-  const carSeatFee = seatNeeded ? 3 * days : 0;
-  const subtotal = rentalFee + carSeatFee;
-  const tax = 0.13 * subtotal;
-  const total = subtotal + tax;
-  const reservationId = Math.floor(Math.random() * 9000) + 1000;
+//// Calculate fees and taxes for the rental
+const rentalFee = days * car.dailyRate;
+const carSeatFee = seatNeeded ? 3 * days : 0;
+const subtotal = rentalFee + carSeatFee;
+const tax = 0.13 * subtotal;
+const total = subtotal + tax;
+const reservationId = Math.floor(Math.random() * 9000) + 1000;
 
+//// Print the reservation receipt
 console.log(`~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`);
-console.log(
-`RECEIPT`)
+console.log(`RECEIPT`);
 console.log(`~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`);
-console.log(`  
+console.log(`
 ID: ${reservationId}
 Car Type: ${car.type}
 License Plate: ${car.licensePlate}
 Rental Days: ${days}
-Car Seat: ${seatNeeded ? "true" : "false"}
+Car Seat: ${seatNeeded ? "true": "false"}
 Subtotal: $${subtotal.toFixed(2)}
 Tax: $${tax.toFixed(2)}
-Total: $${total.toFixed(2)}`);
+Total: $${total.toFixed(2)}
+`);
 console.log(`\n`)
+
+//// Set the car's availability to false
 car.available = false;
 }
 
-const prompt=require('prompt-sync')();
-console.log("\n");
 
-console.log(`~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`);
-console.log(`Welcome to Junayad's car rental`);
-console.log(`~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`);
-console.log(`\n`);
+//// Welcome message to the car rental
+  console.log(`\n`);
+  console.log(`~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`);
+  console.log(`Welcome to Junayad's car rental`);
+  console.log(`~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`);
+  
+//// Prompt user to enter rental details such as car type, rental days, and additional seats needed
+  const prompt = require('prompt-sync')();
+  let carType = prompt(`What type of car do you need? `).toUpperCase();
+  let days = prompt(`For how many days do you need the car? `);
+  let seatNeeded = prompt(`Do you need additional seats? (yes/no) `).toLowerCase() === "yes";
 
+//// Print rental details entered by the user
+  console.log(`\n`);
+  console.log(`Rental Details:`);
+  console.log(`Requested Car Type: ${carType}`);
+  console.log(`Rental Days: ${days}`);
+  console.log(`Car Seat: ${seatNeeded ? "true" : "false"}`);
+  console.log(`\n`);
 
-let carr = prompt(`What type of car you need ? `)
-carType=carr.toUpperCase();
-let days = prompt(`For how many days ? `)
-let seatNeeded = prompt(`Do you need additional seats ? `)
-console.log(`\n`);
-
-
-console.log(
-`Rental Details:
-Requested Car Type: ${carType}
-Rental Days: ${days}
-Car Seat: ${seatNeeded ? "true" : "false"}`);
-
-console.log(`\n`);
-
+//// Call createReservation function with the rental details entered by the user
 createReservation(carType, days, seatNeeded);
